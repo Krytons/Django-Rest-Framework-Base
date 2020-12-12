5b9dd1179bbaaf424f488a08fad05cd0d4f86f72
Bartolomeo_Caruso <48251346+Krytons@users.noreply.github.com>
2020-12-12 12:35:17 +0100

    Completed Observation views

--- apiXamarine/urls.py
+++ apiXamarine/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('register', register_user, name='register'),
     path('login', obtain_auth_token, name='login'),
     path('create_observation', create_observation, name='create_observation'),
-    path('get_all_observation', get_all_observation, name='get_all_observation')
+    path('get_all_observation', get_all_observation, name='get_all_observation'),
+    path('get_user_observation', get_user_observation, name='get_user_observation'),
+    path('update_observation/<int:pk>', update_observation, name='update_observation'),
+    path('delete_observation/<int:pk>', delete_observation, name='delete_observation')
 ]
--- apiXamarine/views.py
+++ apiXamarine/views.py
@@ -1,22 +1,24 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.utils import DatabaseError
 from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
-from djongo.database import DatabaseError
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 
-from apiXamarine.models import ObservedProduct
+from apiXamarine.models import ObservedProduct, AppUser
 from apiXamarine.serializers import RegistrationSerializer, ObservedProductSerializer
 from rest_framework.authtoken.models import Token
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def home(request):
     return Response('This is a test', status=status.HTTP_200_OK)
+
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -54,6 +56,7 @@ def create_observation(request):
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
 def get_all_observation(request):
@@ -64,3 +67,52 @@ def get_all_observation(request):
     except ObjectDoesNotExist:
         return Response({'response': 'There are no observations at the moment'}, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_observation(request):
+    try:
+        observations = ObservedProduct.objects.filter(creator=request.user.id)
+        serializer = ObservedProductSerializer(observations, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except ObjectDoesNotExist:
+        return Response({'response': 'There are no observations for this user at the moment'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_observation(request, pk):
+    try:
+        observation = ObservedProduct.objects.get(id=pk)
+        if observation.creator.email == request.user.email:
+            serializer = ObservedProductSerializer(instance=observation, data=request.data)
+            if serializer.is_valid():
+                if request.user == serializer.validated_data['creator']:
+                    try:
+                        serializer.save()
+                        return Response(serializer.data, status=status.HTTP_200_OK)
+                    except DatabaseError:
+                        return Response({'response':'Duplicate observation'}, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    return Response({'response':'You are not authorized to modify your request with somebody else '
+                                                'email'}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'response': 'You have no permissions to update this observation'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+    except ObjectDoesNotExist:
+        return Response({'response':'This observation does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_observation(request, pk):
+    try:
+        observation = ObservedProduct.objects.get(id=pk)
+        if observation.creator.email == request.user.email:
+            observation.delete()
+            return Response({'response':'Observation successfully deleted'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'You have no permissions to delete this observation'}, status=status.HTTP_401_UNAUTHORIZED)
+    except ObjectDoesNotExist:
+        return Response({'response':'This observation does not exist'}, status=status.HTTP_400_BAD_REQUEST)
